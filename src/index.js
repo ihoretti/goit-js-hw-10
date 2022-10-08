c9f81bfe6943510ac5e2f894ed5266f2d66a3dab
IHOR <97735057+ihoretti@users.noreply.github.com>
2022-10-08 22:38:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,19 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
 import { getRefs } from './js/getRefs';
+
+const refs = getRefs();
+
 const DEBOUNCE_DELAY = 300;
+
+refs.countryInfo.innerHTML = '';
+refs.countryList.innerHTML = '';
+
+Notify.init({
+  width: '280px',
+  position: 'center-top',
+  timeout: 2500,
+  plainText: true,
+  borderRadius: '10px',
+  cssAnimationStyle: 'zoom',
+});
