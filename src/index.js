2371de3f9db5946cbbcd0bb82b50104caf0baa2a
IHOR <97735057+ihoretti@users.noreply.github.com>
2022-10-08 22:43:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,3 +20,82 @@ Notify.init({
   borderRadius: '10px',
   cssAnimationStyle: 'zoom',
 });
+
+refs.searchField.addEventListener(
+  'input',
+  debounce(searchFieldInput, DEBOUNCE_DELAY)
+);
+
+function searchFieldInput(e) {
+  const name = e.target.value.trim();
+  if (name === '') {
+    return clearMarkup();
+  }
+
+  console.log(name);
+  fetchCountries(name)
+    .then(data => {
+      if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        clearMarkup();
+      }
+      if (data.length >= 2 && data.length <= 10) {
+        createCountriesList(data);
+      }
+      if (data.length === 1) {
+        createTargetCountry(data);
+      }
+    })
+    .catch(error => {
+      Notify.failure('Oops, there is no country with that name');
+      clearMarkup();
+    });
+}
+
+function createCountriesList(e) {
+  const makeCountryList = e
+    .map(({ flags, name }) => {
+      return `<li class = 'country-list__item'>
+      <img src="${flags.svg}" width = 50 alt = "flag of ${name.official}">
+      <p class ='country-list__name'>${name.official}</p>
+      </li>`;
+    })
+    .join('');
+  refs.countryList.innerHTML = makeCountryList;
+  refs.countryInfo.innerHTML = '';
+}
+
+function createTargetCountry(e) {
+  const makeCountryList = e
+    .map(({ flags, name }) => {
+      return `<li class = 'country-list__item'>
+      <div class='img-wrap'>
+      <img src="${flags.svg}" width = 100 alt = "flag of ${name.official}">
+      </div>
+      <p class ='country-list__name country-list__name--big'>${name.official}</p>
+      </li>`;
+    })
+    .join('');
+
+  const makeTargetCounrty = e
+    .map(({ capital, population, languages }) => {
+      const langCountry = Object.values(languages).join(', ');
+
+      return `
+              <p><b>Population: </b><span class ='country-info__item'>${population}</span></p>
+              <p><b>Capital: </b><span class ='country-info__item'>${capital}</span></p>
+              <p><b>Languages: </b><span class ='country-info__item'>${langCountry}</span></p>
+          `;
+    })
+    .join('');
+
+  refs.countryInfo.innerHTML = makeTargetCounrty;
+  refs.countryList.innerHTML = makeCountryList;
+}
+
+function clearMarkup() {
+  refs.countryList.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
+}
