015855a91283464e24b53aaed79321b90ab8489e
IHOR <97735057+ihoretti@users.noreply.github.com>
2022-10-08 22:30:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
+import { getRefs } from './js/getRefs';
 const DEBOUNCE_DELAY = 300;
